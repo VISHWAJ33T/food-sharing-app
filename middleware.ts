90b6a66c036053a7a3ff80af56a95c897f3087b6
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-11-12 21:14:39 +0530

    feat: enhance middleware to handle root path and user type redirection

--- middleware.ts
+++ middleware.ts
@@ -8,13 +8,21 @@ export async function middleware(request: NextRequest) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  const publicPaths = ['/', '/login', '/register', '/about'];
+  // console.log('token', token);
+
+  const publicPaths = ['/login', '/register', '/about'];
   const isPublicPath = publicPaths.some((path) =>
     request.nextUrl.pathname.startsWith(path),
   );
 
-  if (!token && !isPublicPath) {
+  const isRootPath = request.nextUrl.pathname === '/';
+
+  if (!token && !isPublicPath && !isRootPath) {
     return NextResponse.redirect(new URL('/login', request.url));
+  }
+  
+  if (token && !token?.userType) {
+    return NextResponse.redirect(new URL('/user-type', request.url));
   }
 
   return NextResponse.next();
